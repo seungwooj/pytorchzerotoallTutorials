1d8e56791e2d9bddbed763a26032314bfa93f5a3
seungwooj <seungwoo.aaron@gmail.com>
2019-05-11 15:09:49 +0900

    [Python] #6 hot fix

--- lec04)back_propagation.py
+++ lec04)back_propagation.py
@@ -1,9 +1,10 @@
 import torch
+from torch.autograd import Variable
 
 x_data = [1.0, 2.0, 3.0]
 y_data = [2.0, 4.0, 6.0]
 
-w_initial = torch.Tensor([1.0])
+w_initial = Variable(torch.Tensor([1.0]), requires_grad=True)
 
 
 # model forward pass
@@ -22,9 +23,10 @@ print("predict (before training)", 4, forward(4, w_initial))
 
 # Training loop
 loss = 0
-for epoch in range(10):
+for epoch in range(100):
     for x_val, y_val in zip(x_data, y_data):
         loss = calculate_loss(x_val, y_val, w_initial)
+        print(loss)
         loss.backward()
         print("\tgrad: ", x_val, y_val, w_initial.grad.data)
         w_initial.data = w_initial.data - 0.01 * w_initial.grad.data
