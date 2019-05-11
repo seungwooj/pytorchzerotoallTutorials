fe0fb10f8d8f409dbe19faa07ec5d368f57343c4
alexkim <hyoungseok.k@gmail.com>
2019-05-11 13:41:54 +0900

    [Python] #5 argumentize weight value

--- lec03)linear_model(gradient).py
+++ lec03)linear_model(gradient).py
@@ -1,35 +1,36 @@
 # give data
 x_data = [1.0, 2.0, 3.0]
 y_data = [2.0, 4.0, 6.0]
-# a random guess for value of w
-w = 1.0
 
 
 # our hypothesis for the linear model
-def forward(x):
+def forward(x, w):
     return x * w
 
 
 # cost(loss) function
-def loss(x, y):
-    y_pred = forward(x)
+def loss(x, y, w):
+    y_pred = forward(x, w)
     return (y_pred - y) * (y_pred - y)
 
 
 # compute gradient
-def gradient(x, y):
+def gradient(x, y, w):
     return 2 * x * (x * w - y)
 
 
+w_initial = 1.0
+
 # Before training
-print("predict (before training)", "4 hours", forward(4))
+print("predict (before training)", "4 hours", forward(4, w_initial))
 # Training loop
 for epoch in range(100):
     for x_val, y_val in zip(x_data, y_data):
-        grad = gradient(x_val, y_val)
-        w = w - 0.01 * grad
+        grad = gradient(x_val, y_val, w_initial)
+        w_new = w_initial - 0.01 * grad
         print("\tgrad: ", x_val, y_val, grad)
-        loss = loss(x_val, y_val)
-    print("progress:", epoch, "w=", w, "loss=", loss)
+        l = loss(x_val, y_val, w_new)
+        w_initial = w_new
+    print("progress:", epoch, "w=", w_initial, "loss=", l)
 # After training
-print("predict (after training)", "4 hours", forward(4))
+print("predict (after training)", "4 hours", forward(4, w_initial))
