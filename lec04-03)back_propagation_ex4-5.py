7f46dcf8d98ee963a2ffb040320f229865ce0ef1
seungwooj <seungwoo.aaron@gmail.com>
2019-05-11 13:16:04 +0900

    [Python] #3 fix convention lec04-03

--- lec04-03)back_propagation_ex4-5.py
+++ lec04-03)back_propagation_ex4-5.py
@@ -11,9 +11,12 @@ w1 = Variable(torch.Tensor([1.0]), requires_grad=True)
 w2 = Variable(torch.Tensor([1.0]), requires_grad=True)
 b = 1.0
 
+
 # our hypothesis for the linear model
 def forward(x):
     return (x * x) * w2 + x * w1 + b
+
+
 # cost(loss) function
 def loss(x, y):
     y_pred = forward(x)
@@ -22,21 +25,20 @@ def loss(x, y):
 
 # Before training
 print("predict (before training)", "4 hours", forward(4).data)
-
 # Training loop
 for epoch in range(100):
     for x_val, y_val in zip(x_data, y_data):
-        l = loss(x_val, y_val)
-        l.backward()
+        loss = loss(x_val, y_val)
+        loss.backward()
         print("\tgrad: ", x_val, y_val, w1.grad.data, w2.grad.data)
         w1.data = w1.data - 0.01 * w1.grad.data
         w2.data = w2.data - 0.01 * w2.grad.data
 
-        #Manually zero the gradients after updating weights
+        # Manually zero the gradients after updating weights
         w1.grad.data.zero_()
         w2.grad.data.zero_()
 
-    print("progress:", epoch, l.data)
+    print("progress:", epoch, loss.data)
 
 # After Training
 print("predict (after training)", 4, forward(4).data)
