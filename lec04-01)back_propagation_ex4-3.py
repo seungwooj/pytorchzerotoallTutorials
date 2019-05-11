40af8619eb540bf3be82df1f5500a1af408aa1b9
seungwooj <seungwoo.aaron@gmail.com>
2019-05-11 14:50:47 +0900

    [Python] #6 change function name

--- lec04-01)back_propagation_ex4-3.py
+++ lec04-01)back_propagation_ex4-3.py
@@ -3,34 +3,36 @@
 
 x_data = [1.0, 2.0, 3.0]
 y_data = [2.0, 4.0, 6.0]
-w = 1
+w_initial = 1
 
 
 # our hypothesis for the linear model
-def forward(x):
+def forward(x, w):
     return x * w
 
 
 # cost(loss) function
-def loss(x, y):
-    y_pred = forward(x)
+def calculate_loss(x, y, w):
+    y_pred = forward(x, w)
     return (y_pred - y) * (y_pred - y)
 
 
 # compute gradient
-def gradient(x, y):
+def calculate_gradient(x, y, w):
     return 2 * x * (x * w - y)
 
 
 # Before training
-print("predict (before training)", 4, forward(4))
+print("predict (before training)", 4, forward(4, w_initial))
 # Training loop
+loss = 0
 for epoch in range(100):
     for x_val, y_val in zip(x_data, y_data):
-        grad = gradient(x_val, y_val)
-        w = w - 0.01 * grad
+        grad = calculate_gradient(x_val, y_val, w_initial)
+        w_update = w_initial - 0.01 * grad
         print("\tgrad: ", x_val, y_val, grad)
         loss = loss(x_val, y_val)
+        w_initial = w_update
 
 # After training
-print("predict (after training)", 4, forward(4))
+print("predict (after training)", 4, forward(4, w_initial))
