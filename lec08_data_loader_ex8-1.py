70fb5f75407035b5b626b0bb3a7680d9f2107202
alexrp <alexrp@company.ai>
2019-05-18 17:34:37 +0900

    [Python] #13 update titanic data loader

--- lec08_data_loader_ex8-1.py
+++ lec08_data_loader_ex8-1.py
@@ -5,27 +5,53 @@
 import torch
 import torch.nn as nn
 import numpy as np
+import re
 from torch.utils.data import Dataset, DataLoader
 
 
 class TitanicDataSet(Dataset):
 
     def __init__(self):
-        xy = np.loadtxt('data-titanic-train.csv', delimiter=',', dtype=np.str)
-        self.len = xy.shape[0]
-        self.x_data = torch.from_numpy(xy[:, 0:-1])
-        self.y_data = torch.from_numpy(xy[:, [-1]])
+        with open("data-titanic-train.csv", "r", encoding="utf-8") as raw:
+            read_line = raw.readlines()
+            self.x_data = []
+            self.y_data = []
+            self.len = len(read_line) - 1
+            for i, line in enumerate(read_line[1:]):
+                replace_name = re.sub('\".+\"', "name", line)
+                split_line = replace_name.replace("\n", "").split(",")
+                survived = int(self.check_nullity((split_line[1])))
+                p_class = int(self.check_nullity(split_line[2]))
+                if split_line[4] == "male":
+                    sex = 0
+                elif split_line[4] == "female":
+                    sex = 1
+                else:
+                    sex = -1
+                age = float(self.check_nullity(split_line[5]))
+                sibsp = int(self.check_nullity(split_line[6]))
+                parch = int(self.check_nullity(split_line[7]))
+                fare = float(self.check_nullity(split_line[9]))
+                self.x_data.append([p_class, sex, age, sibsp, parch, fare])
+                self.y_data.append(survived)
+
+    def check_nullity(self, text):
+        if len(text) == 0:
+            return "0"
+        else:
+            return text
 
     def __getitem__(self, index):
-        return  self.x_data[index], self.y_data[index]
+        return self.x_data[index], self.y_data[index]
 
     def __len__(self):
         return self.len
 
 
 dataSet = TitanicDataSet()
+print(dataSet.len)
 train_loader = DataLoader(dataset=dataSet, batch_size=64, shuffle=True, num_workers=10)
-print(xy)
+# print(train_loader.)
 
 
 
